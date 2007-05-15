852d81495a494a7db0c2275584055bd22fd0b91f
paul <paul@e268981c-6228-0410-a215-dffa596ffb05>
2007-05-15 20:19:12 +0000

    End of autogen hatin'
    git-svn-id: svn://paulbetts.org/gformat@83 e268981c-6228-0410-a215-dffa596ffb05

--- autogen.sh
+++ autogen.sh
@@ -4,4 +4,5 @@ aclocal
 libtoolize --force --copy
 automake -a
 autoconf
-USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes . gnome-autogen.sh
+#USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes . gnome-autogen.sh  # FIXME: This is busted
+./configure --enable-maintainer-mode $*
